f9c63ee1642e765bddd25e65110cc3032063dc83
OFORI Clifford Amanfo <cliffordofori863@gmail.com>
2024-07-06 20:13:13 -0700

    topnav added

---
+++ partials/app-topnav.php
@@ -0,0 +1,5 @@
+<div class="DashboardContentTopNav">
+    <a href="" id="ToggleBtn" title="Toggle Sidebar"><i class="fa-solid fa-bars" aria-hidden="true"></i></a>
+    <div class="Logout">
+        <a href="database/logout.php"><i class="fa-solid fa-power-off"></i>Log-out</a>
+    </div>
